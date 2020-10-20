436fbb08c8e6fbe4225912f0a64dbb427c6c843e
cgresens <cgresens@rochester.rr.com>
2020-10-20 16:27:22 -0400

    Hero work from 10/20

--- HerosApp/HerosLib/Hero.cs
+++ HerosApp/HerosLib/Hero.cs
@@ -2,19 +2,58 @@
 
 namespace HerosLib
 {
-  public class Hero
+  public class Hero // default modifier for class is internal
   {
-    public int id; // value type -> System.int32
-                   // int id = 1 or int id; id = 1 are ways to assign values
-    public string name; // reference type 
 
-    // Constructor
-    public Hero()
-    {
-      id = 1;
-      name = "Superman";
+    #region Old way of creating class members
+    /*    // default modifier for member is private
+        int id; // value type -> System.int32
+                // int id = 1 or int id; id = 1 are ways to assign values
 
-    }
+        public int Id
+        {
+          get
+          {
+            return id;
+          }
+          set
+          {
+            id = value;
+          }
+        }
+
+
+        //public int Id(){get;set;};
+
+        public string name; // reference type 
+
+        // Constructor
+        public Hero()
+        {
+          //id = 1;
+          Id = id;
+          name = "Superman";
+
+        }
+
+        // ctor + tab = constructor shortcut
+
+        public Hero(int id, string name)
+        {
+          this.id = id;
+          this.name = name;
+
+        }*/
+    #endregion
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    //1darray
+    public string[] superPowers = new string[10];
+    // public string[,]  2darr = new string[10,10];    //2d array
+    // public string[,,] 3darr = new string[10,10,10]; //3d array
+    public int[][] jagarr = new int[3][]; // jagged array
 
   }
 }
--- HerosApp/HerosUI/Program.cs
+++ HerosApp/HerosUI/Program.cs
@@ -7,9 +7,50 @@ namespace HerosUI
   {
     static void Main(string[] args)
     {
-      Hero obj = new Hero();
-      Console.WriteLine($"{obj.id} {obj.name}");
 
+
+      #region default constructor
+      //Hero obj = new Hero();
+      //Console.WriteLine($"{obj.id} {obj.name}"); 
+      // can't access variables if they aren't public
+      #endregion
+      #region Parametrized constructor
+      //Hero obj1 = new Hero(2, "Ironman");
+      //Console.WriteLine($"{obj1.id} {obj1.name}"); 
+      // can't access variables if they aren't public
+      #endregion
+      #region Access via Properties
+      /*Console.WriteLine(obj1.Id);
+      obj1.Id = 3;
+      Console.WriteLine($"New Id: {obj1.Id}");
+      
+      */
+      #endregion
+      #region Accessing 1-d arrays
+
+
+      Hero obj = new Hero();
+      /* Console.Write("Please enter your hero's id: ");
+      obj.Id = Int32.Parse(Console.ReadLine());
+      Console.Write("Please enter your hero's name: ");
+      obj.Name = Console.ReadLine();
+      Console.Write("Please enter your hero's first power: ");
+      obj.superPowers[0] = Console.ReadLine();
+
+      Console.Write($"{obj.Id} {obj.Name} {obj.superPowers[0]}");
+   */
+
+      // Jagged Arrays row initializations
+      obj.jagarr[0] = new int[2]; //first column
+      obj.jagarr[1] = new int[3]; //second column
+      obj.jagarr[2] = new int[1]; // third column
+      obj.jagarr[0][0] = 10;
+      obj.jagarr[1][2] = 15;
+      Console.WriteLine(obj.jagarr.Length); // Length is the number of elements of the array
+      Console.WriteLine(obj.jagarr.Rank); // Rank is the dimension of the array
+
+
+      #endregion
     }
   }
 }
