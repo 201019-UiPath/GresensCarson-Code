269069be4e51324edb7441a0390cb33368da35f8
cgresens <cgresens@rochester.rr.com>
2020-10-20 09:56:35 -0400

    HelloWorld

---
+++ Dotnet1/helloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿//1. predifined namespace
+using System;
+
+//1.1 Custom defined namespace
+namespace helloWorld
+{
+  class Program
+  {
+    // execution starts here
+    //command line arguments
+    // vvv
+    static void Main(string[] args)
+    {
+      //Console.WriteLine("Hello World! " + DateTime.Now);
+
+      string name;
+      string lastname;
+      double num; // added this to see if it applied to other types
+      Console.WriteLine("Please enter your first name");
+      name = Console.ReadLine(); // takes users input in string form
+      Console.WriteLine("Please enter your last name");
+      lastname = Console.ReadLine();
+      //Console.WriteLine("Welcome" +name);
+      num = 3;
+      Console.WriteLine($"Welcome {name} {lastname} {num}");
+    }
+  }
+}
