b00bbe5b53d7c41a00ff07da9686d6b28396a061
cgresens <cgresens@rochester.rr.com>
2020-10-21 09:54:38 -0400

    Hero Generics

--- HerosApp/HerosLib/Hero.cs
+++ HerosApp/HerosLib/Hero.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HerosLib
 {
@@ -50,10 +51,68 @@ namespace HerosLib
     public string Name { get; set; }
 
     //1darray
-    public string[] superPowers = new string[10];
+    //public string[] superPowers = new string[10];
+    #region Arrays
     // public string[,]  2darr = new string[10,10];    //2d array
     // public string[,,] 3darr = new string[10,10,10]; //3d array
-    public int[][] jagarr = new int[3][]; // jagged array
+    // int[,,,,,,,] darr = new int[10, 10, 10, 10, 10, 10, 10, 10]; //Nd array follows the pattern
+
+    //public int[][] jagarr = new int[3][]; // jagged array
+    #endregion
+
+    #region List
+    /*     public List<string> superPowers = new List<string>();
+
+        public Hero()
+        {
+          superPowers.Add("Strength");
+          superPowers.Add("Flying");
+          superPowers.Add("Xray Vision");
+          superPowers.Add("Durability");
+
+        }
+        public static List<string> GetDefaultPowers()
+        {
+          Hero h1 = new Hero();
+          return h1.superPowers;
+
+        }
+
+        public void RemoveSuperPower(string sp)
+        {
+          if (superPowers.Contains(sp))
+          {
+            superPowers.Remove(sp);
+          }
+        }
+     */
+    #endregion
+
+
+    public Stack<string> superPowers = new Stack<string>();
+
+    public Hero()
+    {
+      superPowers.Push("Strength");
+      superPowers.Push("Flying");
+      superPowers.Push("Xray Vision");
+      superPowers.Push("Durability");
+
+    }
+    public static Stack<string> GetDefaultPowers()
+    {
+      Hero h1 = new Hero();
+      return h1.superPowers;
+
+    }
+
+    public void RemoveSuperPower()
+    {
+      //as a stack this pops the removes the last power added
+      superPowers.Pop();
+    }
+
 
   }
+
 }
