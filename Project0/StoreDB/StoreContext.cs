29b370ac7ed4c913bf57515ef3c721861da27051
cgresens <cgresens@rochester.rr.com>
2020-11-03 09:20:56 -0500

    Added IRepos

--- Project0/StoreDB/StoreContext.cs
+++ Project0/StoreDB/StoreContext.cs
@@ -7,49 +7,51 @@ using StoreLib.Models;
 
 namespace StoreDB
 {
-    public class StoreContext : DbContext
+  public class StoreContext : DbContext
+  {
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Location> Locations { get; set; }
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Order> Orders { get; set; }
+      if (!(optionsBuilder.IsConfigured))
+      {
+        var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            if(!(optionsBuilder.IsConfigured)) {
-                var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-                var connectionString = configuration.GetConnectionString("StoreDB");
-                optionsBuilder.UseNpgsql(connectionString);
-            }
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder) {
-            
-            modelBuilder.Entity<Order>()
-            .HasOne(e => e.PriceStr)
-            .WithMany("Items")
-            .HasForeignKey(e => e.Id);
-
-            modelBuilder.Entity<Customer>()
-            .HasOne(e => e.Name)
-            .WithMany("Items")
-            .HasForeignKey(e => e.Id);
-
-            modelBuilder.Entity<Product>()
-            .HasOne(e => e.Name)
-            .WithOne("Items")
-            .HasForeignKey("Id");
-
-            modelBuilder.Entity<Employee>()
-            .HasOne(e => e.Name)
-            .WithOne("Id1")
-            .HasForeignKey("ID");
-            
-        }
-
+        var connectionString = configuration.GetConnectionString("StoreDB");
+        optionsBuilder.UseNpgsql(connectionString);
+      }
     }
+
+    // protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+    //     modelBuilder.Entity<Order>()
+    //     .HasOne(e => e.PriceStr)
+    //     .WithMany("Items")
+    //     .HasForeignKey(e => e.Id);
+
+    //     modelBuilder.Entity<Customer>()
+    //     .HasOne(e => e.Name)
+    //     .WithMany("Items")
+    //     .HasForeignKey(e => e.Id);
+
+    //     modelBuilder.Entity<Product>()
+    //     .HasOne(e => e.Name)
+    //     .WithOne("Items")
+    //     .HasForeignKey("Id");
+
+    //     modelBuilder.Entity<Employee>()
+    //     .HasOne(e => e.Name)
+    //     .WithOne("Id1")
+    //     .HasForeignKey("ID");
+
+    // }
+
+  }
 }
